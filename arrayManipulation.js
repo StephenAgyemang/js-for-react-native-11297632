152614d7f988d6b193484639e033d80bf12f9e0c
StephenAgyemang <StephenAsamoahAgyemang@outlook.com>
2024-05-18 02:09:06 +0000

    Task 1 commit

---
+++ arrayManipulation.js
@@ -0,0 +1,17 @@
+function processArray (nums){
+    let result = []
+    for (i in nums){
+        if (nums[i] % 2 === 0){
+            result.push(nums[i] * 2)
+        }
+        else{
+            result.push(nums[i] * 3)
+        }
+    }
+    return result
+}
+
+// task 1 usage example
+nums = [3,5,6,9,0]
+console.log(processArray(nums))
+
